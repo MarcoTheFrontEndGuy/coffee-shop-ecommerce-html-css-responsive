ecff4dcad1b70ac4c6b6194a6898254cd3e7e665
Marco Estors <marco_trabajo@yahoo.com>
2022-05-20 00:42:21 +0200

    changing the design just practising, it's done. typography final details left

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 
-let menuIcon = document.getElementById("menu-btn");
-let navbar = document.querySelector(".header__navbar");         
-
+// ===================================================================================================
 // I want to create pure functions (DRY - Don't Repeat Yourself) 
-// this function can be used by other listeners
-function showHideMenu() {
-    navbar.classList.toggle("active");
-    
-    cartContainer.classList.remove("active");
-    headerSearch.classList.remove("active");
-}
+// the functions below can be used by other listeners
+// ===================================================================================================
 
-menuIcon.addEventListener("click", showHideMenu);
+    let menuIcon = document.getElementById("menu-btn");
+    let navbar = document.querySelector(".header__navbar");         
 
-// ========================================================
-// ========================================================
-// ========================================================
-// ========================================================
+    function showHideMenu() {
+        navbar.classList.toggle("active");
+        
+        cartContainer.classList.remove("active");
+        headerSearch.classList.remove("active");
+    }
+
+    menuIcon.addEventListener("click", showHideMenu);
+
+// ===================================================================================================
+
     const allNavLinks = document.querySelectorAll(".header__navbar__a");
 
     function nav_Link_Click_To_Remove_Cart_Search_MobileMenu() {
@@ -25,65 +26,68 @@ menuIcon.addEventListener("click", showHideMenu);
         headerSearch.classList.remove("active");
     }
 
-
+    // Remove active class from cart, menu and search when you click navbar links
     allNavLinks.forEach((eachLink) => {
         eachLink.addEventListener("click", nav_Link_Click_To_Remove_Cart_Search_MobileMenu);
     });
     
-// ========================================================
-// ========================================================
-// ========================================================
+    // Remove active class from cart, menu and search on scroll 
+    window.addEventListener("scroll", nav_Link_Click_To_Remove_Cart_Search_MobileMenu);
 
-const cartContainer = document.querySelector('.header__cart');
-const cartIcon = document.getElementById("cart-btn");
+// ===================================================================================================
 
-function showHideCart() {
-    cartContainer.classList.toggle("active");
+    const cartContainer = document.querySelector('.header__cart');
+    const cartIcon = document.getElementById("cart-btn");
 
-    navbar.classList.remove("active");
-    headerSearch.classList.remove("active");
-}
+    function showHideCart() {
+        cartContainer.classList.toggle("active");
 
-cartIcon.addEventListener("click", showHideCart);
-// ========================================================
+        navbar.classList.remove("active");
+        headerSearch.classList.remove("active");
+    }
 
+    cartIcon.addEventListener("click", showHideCart);
 
-const headerSearchIcon = document.getElementById("search-btn");
-const headerSearch = document.querySelector(".header__search-form");
+// =====================================================================================================
 
+    const headerSearchIcon = document.getElementById("search-btn");
+    const headerSearch = document.querySelector(".header__search-form");
 
-function showHideSearchBar() {
-    headerSearch.classList.toggle("active");
+    function showHideSearchBar() {
+        headerSearch.classList.toggle("active");
 
-    navbar.classList.remove("active");
-    cartContainer.classList.remove("active");
-}
+        navbar.classList.remove("active");
+        cartContainer.classList.remove("active");
+    }
 
-headerSearchIcon.addEventListener("click", showHideSearchBar);
+    headerSearchIcon.addEventListener("click", showHideSearchBar);
 
-
-
-
-
-
-// ================================================
-// ================================================
+// ===================================================================================================
 
     let btnList = document.querySelectorAll(".btn");
-    
+        
     function testingButton(e) {
         e.preventDefault();
         console.log(e + " element was clicked");
         // return false;
     }
-    
+
     btnList.forEach((eachButton) => {
         eachButton.addEventListener("click", testingButton);
     });
 
 
-// ================================================
-// ================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+
 
 
 
